899c28c571123dd013ae6916d59bf43bdde81b19
ninoslavjaric <jaricninoslav@gmail.com>
2025-11-05 21:25:32 +0100

    improve gha

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+echo "Changing to script directory..."
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-
 cd $SCRIPT_DIR
 
+echo "Loading environment variables from .env..."
 # load env
 source .env
 
+echo "Building Docker image with PORT=$PORT..."
 # build image
-docker buildx build --build-arg PORT=$PORT -t fa .
+docker buildx build --build-arg PORT=$PORT -t fa --load .
 
-# If first argument is 'test', run container and execute test.sh inside the container.
-# Otherwise run container without passing test.sh.
+docker images
+
 if [ "${1:-}" = "test" ]; then
+  echo "Running container and executing test.sh inside the container..."
   docker run -v /tmp/out:/tmp/out -p $PORT:$PORT --env-file .env -t fa test.sh
 else
+  echo "Running container without test.sh..."
   docker run -v /tmp/out:/tmp/out -p $PORT:$PORT --env-file .env -t fa
 fi
